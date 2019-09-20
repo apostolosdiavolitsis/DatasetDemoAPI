aa5d74015ea5bbb7d50cd40e76b28295a114ba94
apostolosdiavolitsis <apostolosdiavolitsis@gmail.com>
2019-09-20 10:02:20 +0300

    Create summple page

--- src/main/java/gr/datasetdemo/api/DatasetDemoApiApplication.java
+++ src/main/java/gr/datasetdemo/api/DatasetDemoApiApplication.java
@@ -2,12 +2,19 @@ package gr.datasetdemo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DatasetDemoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatasetDemoApiApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String info() {
+		return "This is a demo REST API for saving datasets and payloads at a MySQL database";
+	}
 }
