8e4c6f05d430ccb7be6795dc8cb25bd4313bf0c5
apostolosdiavolitsis <apostolosdiavolitsis@gmail.com>
2019-09-20 10:00:13 +0300

    Create Project

---
+++ src/main/java/gr/datasetdemo/api/DatasetDemoApiApplication.java
@@ -0,0 +1,13 @@
+package gr.datasetdemo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetDemoApiApplication.class, args);
+	}
+
+}
