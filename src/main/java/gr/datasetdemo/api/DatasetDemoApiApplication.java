83a2d12c2b3187dd284984b5da3624ab5b74d27b
apostolosdiavolitsis <apostolosdiavolitsis@gmail.com>
2019-09-20 12:58:59 +0300

    Add GET method for datasets

--- src/main/java/gr/datasetdemo/api/DatasetDemoApiApplication.java
+++ src/main/java/gr/datasetdemo/api/DatasetDemoApiApplication.java
@@ -1,18 +1,24 @@
 package gr.datasetdemo.api;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -47,7 +53,7 @@ public class DatasetDemoApiApplication {
 			String sql = "INSERT INTO Payload VALUES (?,?,?)";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, payload.getId());
-			pstmt.setString(2, payload.getCreatDate());
+			pstmt.setString(2, payload.getCreateDate());
 			pstmt.setBinaryStream(3, new ByteArrayInputStream(payload.getBlob()));
 			pstmt.executeUpdate();
 			
@@ -71,6 +77,52 @@ public class DatasetDemoApiApplication {
 		return new ResponseEntity<>("Dataset created successfully", HttpStatus.CREATED);
 	}
 	
-	
+	@GetMapping(value = "/datasets")
+	public ResponseEntity<Object> getDatasets(@RequestParam(required=false,defaultValue="false") boolean loadPayloads){
+				
+		ArrayList<Dataset> datasetList = new ArrayList<Dataset>();
+						
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/datasetdemo?serverTimezone=UTC", "apostolos", "8lfvl94fubBu");
+			String sql = "SELECT * FROM Dataset";
+			Statement stmt = conn.createStatement(); 
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				Dataset dataset = new Dataset();
+				dataset.setId(rs.getInt("id"));
+				dataset.setName(rs.getString("name"));
+				dataset.setDescription(rs.getString("description"));
+				if(loadPayloads) {
+					String sqlPayloads = "SELECT * FROM Payload WHERE id="+rs.getInt("payloadId");
+					Statement stmt2 = conn.createStatement(); 
+					ResultSet rs2 = stmt2.executeQuery(sqlPayloads);
+					Payload payload = new Payload();
+					rs2.next();
+		            payload.setId(rs2.getInt("id"));
+	            	payload.setCreateDate(rs2.getString("createDate"));
+	            	payload.setBlob(rs2.getBinaryStream("blobData").readAllBytes());
+	            	dataset.setPayload(payload);
+	            	//Close ResultSet
+	            	rs2.close();
+	            	stmt2.close();
+				}
+				datasetList.add(dataset);
+			}
+			//Close Connection
+			rs.close();
+			stmt.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<>(datasetList, HttpStatus.OK);
+	}
 	
 }
